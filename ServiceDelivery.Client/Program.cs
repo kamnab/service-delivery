3cf269c45242fe608aa9440477fb771a84557de8
kamnab <k.kamnab@gmail.com>
2025-04-07 19:24:46 +0700

    enable ResponseMode for wasm

--- ServiceDelivery.Client/Program.cs
+++ ServiceDelivery.Client/Program.cs
@@ -36,15 +36,13 @@ builder.Services.AddOidcAuthentication(options =>
     // authentication stack is impacted by a bug that prevents it from correctly extracting
     // authorization error responses (e.g error=access_denied responses) from the URL fragment.
     // For more information about this bug, visit https://github.com/dotnet/aspnetcore/issues/28344.
-
-    // options.ProviderOptions.ResponseMode = "query";
-    options.AuthenticationPaths.RemoteRegisterPath = "https://localhost:44313/Identity/Account/Register";
+    options.ProviderOptions.ResponseMode = "query";
+    options.AuthenticationPaths.RemoteRegisterPath = $"{options.ProviderOptions.Authority}Identity/Account/Register";
 
     // Add the "roles" (OpenIddictConstants.Scopes.Roles) scope and the "role" (OpenIddictConstants.Claims.Role) claim
     // (the same ones used in the Startup class of the Server) in order for the roles to be validated.
     // See the Counter component for an example of how to use the Authorize attribute with roles
     // options.ProviderOptions.DefaultScopes.Add("roles");
-
     options.UserOptions.RoleClaim = "role";
 
 });
