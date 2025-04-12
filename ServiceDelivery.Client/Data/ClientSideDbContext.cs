7b792fdc4c18e9a5cc3ade9c73d8ef7ee5260571
kamnab <k.kamnab@gmail.com>
2025-04-12 22:05:12 +0700

    SeedData runs only in Program.cs

--- ServiceDelivery.Client/Data/ClientSideDbContext.cs
+++ ServiceDelivery.Client/Data/ClientSideDbContext.cs
@@ -14,11 +14,5 @@ internal class ClientSideDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        // Seed data here
-        modelBuilder.Entity<TodoItem>().HasData(
-            new TodoItem { Id = 1, Name = "Wash the car", IsCompleted = false },
-            new TodoItem { Id = 2, Name = "Write a Blazor app", IsCompleted = true },
-            new TodoItem { Id = 3, Name = "Read EF Core docs", IsCompleted = false }
-        );
     }
 }
