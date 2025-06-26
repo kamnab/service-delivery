3a269ecfbdd2749917ba7684a3b69989237b7a2d
kamnab <k.kamnab@gmail.com>
2025-06-26 20:29:58 +0700

    .

--- ServiceDelivery.ProfileService/Program.cs
+++ ServiceDelivery.ProfileService/Program.cs
@@ -46,6 +46,12 @@ builder.Services.AddAuthentication(options =>
     options.Scope.Add("sdc-api");
 
     // Optional: configure token validation, events, etc.
+    // Optional: Event to force override RedirectUri if needed
+    options.Events.OnRedirectToIdentityProvider = context =>
+    {
+        context.ProtocolMessage.RedirectUri = "https://odi-profile.codemie.dev/signin-oidc";
+        return Task.CompletedTask;
+    };
 });
 
 builder.Services.ConfigureApplicationCookie(options =>
