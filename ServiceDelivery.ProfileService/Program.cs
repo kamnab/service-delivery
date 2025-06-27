12f93e43e38ebb0647fbdb23cccd4763acafc07a
kamnab <k.kamnab@gmail.com>
2025-06-27 08:18:07 +0700

    .

--- ServiceDelivery.ProfileService/Program.cs
+++ ServiceDelivery.ProfileService/Program.cs
@@ -7,10 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 // builder.WebHost.UseUrls("http://0.0.0.0:80");
 // builder.WebHost.ConfigureKestrel(options =>
 // {
-//     options.ListenAnyIP(80);
-//     options.ListenAnyIP(443, listenOptions =>
+//     options.ListenAnyIP(5501, listenOptions =>
 //     {
-//         listenOptions.UseHttps(Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path"), Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password"));
+//         listenOptions.UseHttps(
+//             Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path"),
+//             Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password"));
 //     });
 // });
 
